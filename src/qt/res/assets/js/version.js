89ee0682947bca0da5fc36b93db857d15761c2d5
Blaz <blaz.blokar@lucis.si>
2017-08-02 13:33:32 +0200

    new fixes for input fields on windows

--- src/qt/res/assets/js/version.js
+++ src/qt/res/assets/js/version.js
@@ -1,9 +1,1 @@
-/**
- * Created by makoric on 12.7.2017.
- */
-var input = document.createElement("input");
-input.type = "hidden";
-input.id= "new-version-element";
-input.value= "v3.0.0.1"; // LEOCoin version number
-var body=document.getElementsByTagName('body')[0];
-body.appendChild(input);
+var new_production_version='v3.0.6.0';
